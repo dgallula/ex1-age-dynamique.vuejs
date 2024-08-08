68267bbc8c01a0dd34950aabcc1815df8557858a
david <dgallula@gmail.com>
2024-08-08 16:02:38 +0300

    first commit

---
+++ ex1-vuejs/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="ul">liste</div>
+  <ul>
+    <li> {{ persona.firstname }}</li>
+    <li> {{ persona.lastname }}</li>
+    <li> {{ persona.age }}</li>
+
+    <button @click="randomAge">change age</button>
+  </ul>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const persona = ref({
+  firstname: 'John',
+  lastname: 'Doe',
+  age: 30
+})
+function randomAge() {
+  persona.value.age = Math.floor(Math.random() * 100)
+}
+</script>
+
+<script>
+function randomAge() {
+  persona.value.age = Math.floor(Math.random() * 100)
+}
+</script>
+
+
+
+<style>
+/* Ton style CSS */
+</style>
+
+
+
